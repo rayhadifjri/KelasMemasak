a385594edb7c9c313a7b29e554ab4fd7d56d6276
Gutri Rahmad Zuwa <90978004+gutri09@users.noreply.github.com>
2022-05-18 22:03:56 +0700

    Delete rumbud.js

--- rumbud.js
+++
@@ -1,7 +0,0 @@
-function show(anything) {
-    document.querySelector('.bahasa').value = anything;
-}
-Let dropdown_bahasa = document.querySelector('.dropdown_bahasa');
-dropdown_bahasa.onclick = function({
-    dropdown_bahasa.classList.toggle('active');
-})
